9c200c0a9bdb96e4a094146de7a74ca2b5bd7ac6
jeongeunii <961jeun@naver.com>
2021-08-23 00:36:56 +0900

    게시글 수정/조회 API 만들기

---
+++ src/main/java/com/jeuni/example/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.jeuni.example.springboot.web.dto;
+
+import com.jeuni.example.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
