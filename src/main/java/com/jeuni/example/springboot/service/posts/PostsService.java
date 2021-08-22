a9beb415c7d6b8ee6bd9c1f4f5fb0cb2d18308c5
jeongeunii <961jeun@naver.com>
2021-08-22 22:55:48 +0900

    게시글 등록 API 만들기

---
+++ src/main/java/com/jeuni/example/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.jeuni.example.springboot.service.posts;
+
+import com.jeuni.example.springboot.domain.posts.PostsRepository;
+import com.jeuni.example.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
