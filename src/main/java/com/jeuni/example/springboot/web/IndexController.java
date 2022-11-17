c8199ee8e34a738283dcf0d7402cceed7b9f9af5
jeongeunii <961jeun@naver.com>
2022-11-17 22:10:19 +0900

    MBILL Project Final Commit

--- src/main/java/com/jeuni/example/springboot/web/IndexController.java
+++ src/main/java/com/jeuni/example/springboot/web/IndexController.java
@@ -4,10 +4,12 @@ import com.jeuni.example.springboot.service.JobsService;
 import com.jeuni.example.springboot.service.PostsService;
 import com.jeuni.example.springboot.service.SamplesService;
 import com.jeuni.example.springboot.service.TctService;
+import com.jeuni.example.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -18,44 +20,50 @@ public class IndexController {
     private final SamplesService samplesService;
     private final TctService tctService;
 
-//    @GetMapping("/")
-//    public String index(Model model) {
-//        model.addAttribute("posts", postsService.findAllDesc());
-//        return "index";
-//    }
-//
-//    @GetMapping("/posts/calendar")
-//    public String postsCalendar(Model model) {
-//        model.addAttribute("jobs", jobsService.findAllJobs());
-//        return "posts-calendar";
-//    }
-//
-//    @GetMapping("/posts/data")
-//    public String postsData(Model model) {
-//        model.addAttribute("samples", samplesService.findAllSamples());
-//        return "posts-data";
-//    }
-
-    @GetMapping("/posts/table")
-    public String postsTable(Model model) {
-        model.addAttribute("tctSamComs", tctService.findAllTctSamComs());
-        model.addAttribute("tctSamComCount", tctService.findCountOfTctSamComs());
-        model.addAttribute("tctBillSoapCount", tctService.findCountOfTctBillSoaps());
-        model.addAttribute("tctSamComTotCount", tctService.findTotCountOfTctSamComs());
-        model.addAttribute("tctBillSoapTotCount", tctService.findTotCountOfTctBillSoaps());
-        return "posts-table";
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
     }
 
-//    @GetMapping("/posts/save")
-//    public String postsSave() {
-//        return "posts-save";
+    @GetMapping("/posts/calendar")
+    public String postsCalendar(Model model) {
+        model.addAttribute("jobs", jobsService.findAllJobs());
+        return "posts-calendar";
+    }
+
+    @GetMapping("/posts/schedule")
+    public String postsSchedule(Model model) {
+        model.addAttribute("jobs", jobsService.findAllJobs());
+        return "posts-schedule";
+    }
+
+    @GetMapping("/posts/data")
+    public String postsData(Model model) {
+        model.addAttribute("samples", samplesService.findAllSamples());
+        return "posts-data";
+    }
+
+//    @GetMapping("/posts/table")
+//    public String postsTable(Model model) {
+//        model.addAttribute("tctSamComs", tctService.findAllTctSamComs());
+//        model.addAttribute("tctSamComCount", tctService.findCountOfTctSamComs());
+//        model.addAttribute("tctBillSoapCount", tctService.findCountOfTctBillSoaps());
+//        model.addAttribute("tctSamComTotCount", tctService.findTotCountOfTctSamComs());
+//        model.addAttribute("tctBillSoapTotCount", tctService.findTotCountOfTctBillSoaps());
+//        return "posts-table";
 //    }
-//
-//    @GetMapping("/posts/update/{id}")
-//    public String postsUpdate(@PathVariable Long id, Model model) {
-//        PostsResponseDto dto = postsService.findById(id);
-//        model.addAttribute("post", dto);
-//
-//        return "posts-update";
-//    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
 }
